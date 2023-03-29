72d9ce5d210367d2ae2b68b684ac2b94cb229ad1
Rukayat Balogun <74791083+rukayat-balogun@users.noreply.github.com>
2023-03-29 11:32:04 +0100

    Just a sample
    initial commit

---
+++ python2.py
@@ -0,0 +1,50 @@
+import math
+from math import sqrt, acos, ceil, floor
+number = 5  #integer
+
+number = 5.0 #float
+
+number = '5' #string
+
+number = "5"
+#this is not supposed to be displayed
+#jiehfhofbeofboi;f
+#sijhfiuefouh;
+"""
+hfhrbcbwc3hfhsoc;
+cclidcirvbiucbdcs
+cdbcuieguefhoc\cbehc
+jcch9rifhgi3fgi3
+
+"""
+number = True #Bool or Boolean
+
+
+number = False
+
+
+
+num1 = 5
+
+num2 = 4
+
+addNum = num1 + num2 #adds up numbers
+#print(addNum)
+
+
+
+subtNum = num1 - num2
+
+#print(subtNum)
+
+#print(math.sqrt(addNum))
+
+#print(sqrt(16))
+
+
+sentence = "Today is Wednesday."
+print(sentence)
+print(type(sentence))
+
+
+
